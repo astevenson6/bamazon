0f35e50c6c96dd81fa60a4bd01239a611d4c8a0e
astevenson6 <astevenson6@yahoo.com>
2019-07-22 22:50:34 -0700

    used ice cream example to finish sql doc and customer.js

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,6 @@ CREATE Table products(
     stock_quantity INT(100) NOT NULL,
     PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("1", keyboard, electronics, 40.00, 75), ("2", mouse, electronics, 25.00, 50), ("3", headset, electronics, 25.00, 50), ("4", monitor, electronics, 150.00, 50), ("5", desk, furniture, 200.00, 25), ("6", shirt, clothing, 20.00, 100), ("7", pants, clothing, 40.00, 75), ("8", milk, groceries, 5.00, 75), ("9", oranges, produce, 10.00, 100), ("10", bike, sports, 150.00, 20);
